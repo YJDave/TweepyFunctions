f2308c53c19a85f6c63b2c93824af5a4f27e15a5
Yashashvi Dave <yashashvidave@gmail.com>
2017-07-16 00:31:19 +0530

    1 - Direct Messages Methods

--- third.py
+++ third.py
@@ -20,17 +20,17 @@ def write_into_file_func(func):
 	myfile.write("\n\n\n")
 
 
-def write_into_file_DirectMessage(direct_message, flag):
+def write_into_file_DirectMessage(direct_message):
 
 	myfile.write("\nMessage : \n")
 	
-	if flag:
-		myfile.write("\n--> Sender :")
-		write_into_file_User(str((direct_message.sender).name), direct_message.sender, False)
 	
-	if flag is False:
-		myfile.write("\n--> Recipient : ")
-		write_into_file_User(str(direct_message.recipient.name), direct_message.recipient, False)	
+	myfile.write("\n--> Sender :")
+	write_into_file_User(str((direct_message.sender).name), direct_message.sender, False)
+	
+	
+	myfile.write("\n--> Recipient : ")
+	write_into_file_User(str(direct_message.recipient.name), direct_message.recipient, False)	
 	
 	myfile.write(str("\n--> Message Id : "+direct_message.id_str+"\n"))
 	myfile.write(str("Time : "+str(direct_message.created_at)+"\n"))
@@ -68,12 +68,10 @@ try:
 	direct_messages_of_me = api.direct_messages()
 
 	write_into_file_func("direct_messages")
-
-	myfile.write("\n--> Recipient : ")
-	write_into_file_User(str(direct_message.recipient.name), direct_message.recipient, False)	
+	
 
 	for i in direct_messages_of_me:
-		write_into_file_DirectMessage(i, True)
+		write_into_file_DirectMessage(i)
 
 	print("Successfully implemented direct_messages")
 
@@ -87,15 +85,51 @@ try:
 
 	write_into_file_func("sent_direct_messages")
 
-	myfile.write("\n--> Sender :")
-	write_into_file_User(str((direct_message.sender).name), direct_message.sender, False)
-	
 
 	for i in sent_direct_messages_of_me:
-		write_into_file_DirectMessage(i, False)
+		write_into_file_DirectMessage(i)
 
 
 	print("Successfully implemented sent_direct_messages")
+
+
+###############---------send_direct_message-------#####################
+#sent direct message to specific user from authenticating user
+
+
+#@@@@@@@@@@@try user which is not friend
+#@@@@@@@@@@@code:150 You can't send the message to users who are not following you
+
+	user = "thehdrug"
+	sent_direct_message_to = api.send_direct_message(user,text="Hello..")
+
+	write_into_file_func("send_direct_message")
+
+	
+	myfile.write("Message Send to User.. Message Info : \n")
+	write_into_file_DirectMessage(sent_direct_message_to)
+
+
+	print("Successfully implemented send_direct_message")
+
+
+###############---------destroy_direct_message-------#####################
+#Destroy a direct message. Authenticating user must be the recipient of the direct message.
+
+
+	# message_id = ''
+	# destroy_direct_message_to = api.destroy_direct_message(message_id)
+
+	# write_into_file_func("destroy_direct_message")
+
+	
+	# myfile.write("Message Sent to User.. Message Info : \n")
+	# write_into_file_DirectMessage(destroy_direct_message_to)
+
+
+	# print("Successfully implemented destroy_direct_message")
+
+
 
 
 	myfile.close()
