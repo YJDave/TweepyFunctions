918e90e833c4faf57f882dea85bf5e00bf3a29ef
Yashashvi Dave <yashashvidave@gmail.com>
2017-07-27 17:49:03 +0530

    Appropiate Names

---
+++ FriendshipMethods.py
@@ -0,0 +1,173 @@
+def write_into_file_Friendhsip(friendship):
+
+	myfile.write("\n--> Friendhsip")
+	myfile.write("\nUser1 Name : "+friendship[0].screen_name)
+	myfile.write("\nUser1 ID : "+friendship[0].id_str)
+	myfile.write("\nUser2 Name : "+friendship[1].screen_name)
+	myfile.write("\nUser2 ID : "+friendship[1].id_str)
+	myfile.write("\nIs "+friendship[0].screen_name+" following to "+friendship[1].screen_name+" "+str(friendship[1].followed_by))
+	myfile.write("\nIs "+friendship[1].screen_name+" following to "+friendship[0].screen_name+" "+str(friendship[0].followed_by))
+
+def write_into_file_User(user_name, user_info, flag):
+
+	myfile.write("\n--> "+str(user_name)+"\n")
+	myfile.write(str("Name : "+str(user_info.name)+"\n"))	
+	myfile.write(str("User Id : "+user_info.id_str+"\n"))
+	myfile.write(str("User Name : "+str(user_info.screen_name)+"\n"))
+	
+	if flag:
+		
+		myfile.write(str("Created at : +"+str(user_info.created_at)+"\n"))
+		myfile.write(str("No of Followers : "+str(user_info.followers_count)+"\n"))
+		myfile.write(str("No of Following : "+str(user_info.friends_count)+"\n"))
+		myfile.write(str("Description : "+str(user_info.description)+"\n"))
+
+
+def write_into_file_func(func):
+
+	myfile.write("\n\n\n")
+	myfile.write("##################-----"+func+"------------##################")
+	myfile.write("\n\n\n")
+
+
+from tweepy import (
+	OAuthHandler,
+	API
+)
+
+consumer_key = '461XyHMzRdFHY8ZxN8XArPkoG'
+consumer_secret = 'CFMsYLcFOUCEvJ6f9wtAmGjSoivtU5Dqt2NHn8ZfdRKu3fhWh3'
+
+access_token = '794909294168854528-Gx1rbjo7iCGIQ5tGslCPuyCbDEhmD9h'
+access_secret = 'jvtsr4l755InF67FIdrcOGPO1rez2xLWQ0OYkpzsoJtAW'
+
+callback_url = "http://google.com"
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+#tweeter.api take arguments like auth_handler, host, search_host,..
+api = API(auth)
+
+myfile = open('twetdata.csv', 'a')
+
+try:
+
+
+###############---------followers_ids-------#####################
+#returns id of users which are followed by specific user
+#return type is list of integer
+
+	user = "thehdrug"
+
+	all_followers_of_user = api.followers_ids(user)
+
+	write_into_file_func("followers_ids")
+	myfile.write("\nName & ID of Followers of "+user+" are : \n")
+
+	for i in all_followers_of_user:
+		myfile.write(((api.get_user(i)).name)+"\t\t"+str(i)+"\n")
+
+	print("Successfully implemented followers_ids")
+
+
+
+
+###############---------friends_ids-------#####################
+#returns id of followers of specific user
+#return type is list of integer
+
+	user = "yashashvi_dave"
+
+	all_followers_of_user = api.friends_ids(user)
+
+	write_into_file_func("friends_ids")
+	myfile.write("\nName & ID of Users followed by "+user+" are : \n")
+
+	
+	for i in all_followers_of_user:
+		myfile.write(((api.get_user(i)).name)+"\t\t"+str(i)+"\n")
+
+	print("Successfully implemented friends_ids")
+
+
+###############---------show_friendship-------#####################
+#Returns detailed information about the relationship between two users.
+#returns tuple of two Friendship Object
+
+	user1 = 'architad8'
+	user2 = 'yashashvi_dave'
+
+	friendship = api.show_friendship(source_id=user1,
+									source_screen_name = user1, 
+									target_id=user2,
+									target_screen_name = user2)
+
+	write_into_file_func("show_friendship")
+
+	write_into_file_Friendhsip(friendship)
+
+	print("Successfully implemented show_friendship")
+
+
+###############---------create_friendship-------#####################
+#Start following specific user
+#return User object
+
+	user = 'architad8'
+
+	start_follow = api.create_friendship(user)
+
+	write_into_file_func("create_friendship")
+
+	myfile.write("\nAuthenticated User Start Following to This User :\n")
+	
+	write_into_file_User(user, start_follow, False)
+
+	print("Successfully implemented create_friendship")
+
+
+###############---------destroy_friendship-------#####################
+#Stop following specific user
+#return User object
+
+	user = 'Medium'
+
+	stop_follow = api.destroy_friendship(user)
+
+	write_into_file_func("destroy_friendship")
+
+	myfile.write("\nAuthenticated User Stop Following to This User :\n")
+	
+	write_into_file_User(user, stop_follow, False)
+
+	print("Successfully implemented destroy_friendship")
+
+
+# ###############---------exists_friendship-------#####################
+# #Check if user1 is followint user2 or not
+# #return True or False
+
+# 	user2 = "Medium"
+# 	user1 = "architad8"
+
+# 	check_follow = api.exists_friendship(user1, user2)
+
+# 	write_into_file_func("exists_friendship")
+
+# 	myfile.write("\nIs "+user1+" is following to "+user2+"  user? : \n")
+
+# 	myfile.write("\n"+check_follow+"\n")
+
+# 	print("Successfully implemented exist_friendship")
+
+
+
+
+
+	myfile.close()
+except Exception as e:
+
+	print("Error Occured. "+ str(e) +  " Try Again. ")
+
+
